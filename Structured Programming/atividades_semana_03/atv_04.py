20ede70670f24df7f46aa87708b59c47c299a3f7
Clenilson Willames Alves Feitosa <willamesclenilson@gmail.com>
2026-08-22 09:26:59 -0300

    Refactor volume difference calculation with prompts

--- Structured Programming/atividades_semana_03/atv_04.py
+++ Structured Programming/atividades_semana_03/atv_04.py
@@ -1,10 +1,15 @@
+#===================================
+# Autor [clenilson willames alves feitosa]
+# status [stable]
+# Version [1.0]
+#===================================
 
 
-vol_atual = input().strip()
-vol_desejado = input().strip()
+vol_atual = input("Digite o volume atual: ").strip()
+vol_desejado = input("Digite o volume desejado: ").strip()
 
 
 diferença = int(vol_desejado) - int(vol_atual) 
 
 
-print(diferença)
+print(f"A diferença de volumes é: {diferença}")
