a668630a48179e293a8d58656a994ecabb9d9336
Clenilson Willames Alves Feitosa <willamesclenilson@gmail.com>
2026-08-22 09:23:51 -0300

    Refactor input prompt and add author info

--- Structured Programming/atividades_semana_03/atv_01.py
+++ Structured Programming/atividades_semana_03/atv_01.py
@@ -1,7 +1,10 @@
+#===================================
+# Autor [clenilson willames alves feitosa]
+# status [stable]
+# Version [1.0]
+#===================================
 
-
-
-x = input().strip()
+x = input("Digite um número inteiro: ").strip()
 
 resultado = int(x) * "miau"
 
