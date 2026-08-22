e8009504ba1313ae757830a90c494488703cac89
Clenilson Willames Alves Feitosa <willamesclenilson@gmail.com>
2026-08-22 09:20:58 -0300

    Enhance input prompt and output format in atv_04.py
    Added user prompt and formatted output message.

--- Structured Programming/atividades_semana_02/atv_04.py
+++ Structured Programming/atividades_semana_02/atv_04.py
@@ -4,10 +4,10 @@
 # Version [1.0]
 #===================================
 
-valor = input().strip()
+valor = input("Digite a quantidade de dias que o dragão está solto: ").strip()
 
 n_ovelhas = 0.5 # número de ovelhas por dia
 
 devoradas = float(valor) * n_ovelhas
 
-print(devoradas)
+print(f"Foram devoradas {devoradas} ovelhas")
