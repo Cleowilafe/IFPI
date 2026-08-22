e2ccd7075d7407375f19a05efd2e1c3046c7f341
Clenilson Willames Alves Feitosa <willamesclenilson@gmail.com>
2026-08-22 09:29:16 -0300

    Update atv_05.py

--- Structured Programming/atividades_semana_03/atv_05.py
+++ Structured Programming/atividades_semana_03/atv_05.py
@@ -1,13 +1,15 @@
+#===================================
+# Autor [clenilson willames alves feitosa]
+# status [stable]
+# Version [1.0]
+#===================================
 
 
-
-s_marte = input().strip()
-vel_nave = input().strip()
-
+s_marte = input("Digite a distância até marte (km): ").strip()
+vel_nave = input("Digite a velocidade média da nave (km/h): ").strip()
 
 tempo = float(s_marte) / float (vel_nave)
 
-
-print (tempo)
+print (f"Você chegará em marte em {tempo} horas")
 
 
