196b3f1ecec139e01b6bee886525a838a9efb443
Clenilson Willames Alves Feitosa <willamesclenilson@gmail.com>
2026-08-22 09:07:46 -0300

    Add metadata and prompt for user name input

--- Structured Programming/atividades_semana_01/atv_02.py
+++ Structured Programming/atividades_semana_01/atv_02.py
@@ -1,2 +1,9 @@
-nome = input().strip()
+#===================================
+# Autor [clenilson willames alves feitosa]
+# status [stable]
+# Version [1.0]
+#===================================
+
+nome = input("Digite seu nome: ").strip()
+
 print(f"Seja bem-vindo(a), {nome}!")
