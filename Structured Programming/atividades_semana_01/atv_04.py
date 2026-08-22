888500dc7a038ab7e15a2bebae306e16a4c41838
Clenilson Willames Alves Feitosa <willamesclenilson@gmail.com>
2026-08-22 09:15:23 -0300

    Refactor input prompts and add metadata comments

--- Structured Programming/atividades_semana_01/atv_04.py
+++ Structured Programming/atividades_semana_01/atv_04.py
@@ -1,4 +1,10 @@
-a = float(input())
-b = float(input())
+#===================================
+# Autor [clenilson willames alves feitosa]
+# status [stable]
+# Version [1.0]
+#===================================
+
+a = float(input("Digite um número: ").strip())
+b = float(input("Digite um número: ").strip())
 soma = a + b
-print(f"{soma:.2f}")
+print(f"A soma é: {soma:.2f}")
