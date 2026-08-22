b9e8885bb9a09a83e6143a47edc548fe71c63123
Clenilson Willames Alves Feitosa <willamesclenilson@gmail.com>
2026-08-22 08:42:38 -0300

    Create atv1.py

---
+++ Structured Programming/atividades_semana_02/atv1.py
@@ -0,0 +1,15 @@
+#===================================
+# Autor [clenilson willames alves feitosa]
+# status [stable]
+# Version [1.0]
+#===================================
+
+# Lendo os volumes das duas substâncias
+volume1 = float(input())
+volume2 = float(input())
+
+# Calculando o total da poção
+total = volume1 + volume2
+
+# Exibindo o resultado formatado
+print( f"{total}")
