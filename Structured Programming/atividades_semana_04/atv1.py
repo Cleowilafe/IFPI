5be5f2df8cc5852ae2973fcb10e5b9419dfe438e
Clenilson Willames Alves Feitosa <willamesclenilson@gmail.com>
2026-09-05 19:00:00 -0300

    Add date input and display functionality in atv1.py

---
+++ Structured Programming/atividades_semana_04/atv1.py
@@ -0,0 +1,8 @@
+#Inserir variavel dias
+dia = int(input('Digite o dia '))
+#Inserir variavel mes
+mes = int(input('Digite o mês '))
+#Inserir variavel ano
+ano = int(input('Digite o ano '))
+#Imprimir na tela a data
+print(f'A data: {dia}/{mes}/{ano}')
