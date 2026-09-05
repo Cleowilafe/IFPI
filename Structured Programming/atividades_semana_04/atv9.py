8e3eea58a0a4117ee710f68b71af7bc8025de1c9
Clenilson Willames Alves Feitosa <willamesclenilson@gmail.com>
2026-09-05 19:04:39 -0300

    Update atv9.py

--- Structured Programming/atividades_semana_04/atv9.py
+++ Structured Programming/atividades_semana_04/atv9.py
@@ -1,8 +1,3 @@
-#Variavel a recebe um valor numerico
-a = float(input('Digite primeiro número '))
-#Variavel b recebe um valor numerico 
-b = float(input('Digite segundo número'))
-#Variavel c recebe um valor numerico 
-c = float(input('Digite terceiro número'))
-#Imprime na tela média dos variaveis a, b, c
-print(f'Média desses número é {(a+b+c)/3}')
+entrada = int(input("Digite um número inteiro: "))
+
+print(entrada > 100)
