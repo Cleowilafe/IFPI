8485c2d2fe1603f6165dfe4f09530a6177a98878
Clenilson Willames Alves Feitosa <willamesclenilson@gmail.com>
2026-09-13 22:13:29 -0300

    Update atv3.py

--- Structured Programming/atv_semana_5/atv3.py
+++ Structured Programming/atv_semana_5/atv3.py
@@ -1,14 +1,20 @@
 def min_para_hrs(qtd_minutos):
     horas = qtd_minutos // 60
     minutos = qtd_minutos % 60
+
     return f"{horas}:{minutos:02d}"
 
 
-# INPUT
-minutos = int(input("Digite a quantidade de minutos: "))
+def main():
+    # INPUT
+    minutos = int(input("Digite a quantidade de minutos: "))
 
-# PROCESSAMENTO
-horas = min_para_hrs(minutos)
+    # PROCESSAMENTO
+    horas = min_para_hrs(minutos)
 
-# OUTPUT
-print(f"Horário convertido: {horas}")
+    # OUTPUT
+    print(f"Horário convertido: {horas}")
+
+
+if __name__ == "__main__":
+    main()
