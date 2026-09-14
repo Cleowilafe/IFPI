5df03a3c3adf13130e8c87de6d0e1b44f0c4824d
Clenilson Willames Alves Feitosa <willamesclenilson@gmail.com>
2026-09-13 22:13:08 -0300

    Update atv2.py

--- Structured Programming/atv_semana_5/atv2.py
+++ Structured Programming/atv_semana_5/atv2.py
@@ -6,13 +6,18 @@ def perimetro_quadrado(lado):
     return lado * 4
 
 
-# INPUT
-valor_lado = float(input("Digite o valor do lado: "))
+def main():
+    # INPUT
+    valor_lado = float(input("Digite o valor do lado: "))
 
-# PROCESSAMENTO
-area = area_quadrado(valor_lado)
-perimetro = perimetro_quadrado(valor_lado)
+    # PROCESSAMENTO
+    area = area_quadrado(valor_lado)
+    perimetro = perimetro_quadrado(valor_lado)
 
-# OUTPUT
-print("Área do quadrado: %10.4f" % area)
-print("Perímetro do quadrado: %10.4f" % perimetro)
+    # OUTPUT
+    print(f"Área do quadrado: {area:10.4f}")
+    print(f"Perímetro do quadrado: {perimetro:10.4f}")
+
+
+if __name__ == "__main__":
+    main()
