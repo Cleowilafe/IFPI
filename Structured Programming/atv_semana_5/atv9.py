dd0d24a99bd2a89f4368993509c55b8e1a0f1c9c
Clenilson Willames Alves Feitosa <willamesclenilson@gmail.com>
2026-09-13 22:11:14 -0300

    Create atv9.py

---
+++ Structured Programming/atv_semana_5/atv9.py
@@ -0,0 +1,28 @@
+def verificador(x):
+    return (
+        x == "A" or x == "B" or x == "C" or x == "D" or
+        x == "E" or x == "F" or x == "G" or x == "H" or
+        x == "I" or x == "J" or x == "K" or x == "L" or
+        x == "M" or x == "N" or x == "O" or x == "P" or
+        x == "Q" or x == "R" or x == "S" or x == "T" or
+        x == "U" or x == "V" or x == "W" or x == "X" or
+        x == "Y" or x == "Z" or
+        x == "0" or x == "1" or x == "2" or x == "3" or
+        x == "4" or x == "5" or x == "6" or x == "7" or
+        x == "8" or x == "9"
+    )
+
+
+def main():
+    # INPUT
+    x = input("Digite um caractere: ").strip().upper()
+
+    # PROCESSAMENTO
+    resultado = verificador(x)
+
+    # OUTPUT
+    print(f"É uma letra ou número? {resultado}")
+
+
+if __name__ == "__main__":
+    main()
