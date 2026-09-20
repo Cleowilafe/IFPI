1929d946dff93ddc3a02d2259a378b638cb84aba
Clenilson Willames Alves Feitosa <willamesclenilson@gmail.com>
2026-09-19 22:21:50 -0300

    Update name input and character count display

--- Structured Programming/atv_semana_06/atv1.py
+++ Structured Programming/atv_semana_06/atv1.py
@@ -1,10 +1,17 @@
+#Entrada de dados
+nome = input("Escreva um nome: ")
+
+#Processamento de dados
+
 def ler(nome):
     nome = len(nome)
     return nome
 
-nome = input()
 nome = ler(nome)
+
+#Saída de dados
 def main():
-    print(nome)
+    print(f"O nome possui {nome} caracteres.")
+
 if __name__ == "__main__":
     main()
