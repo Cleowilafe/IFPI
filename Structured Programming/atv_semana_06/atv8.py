75f2e1e6716a67610b8be9f929042f4c06f597a0
Clenilson Willames Alves Feitosa <willamesclenilson@gmail.com>
2026-09-19 22:25:48 -0300

    Update atv8.py

--- Structured Programming/atv_semana_06/atv8.py
+++ Structured Programming/atv_semana_06/atv8.py
@@ -1,16 +1,18 @@
 #Entrada de dados
-valor1 = float(input())
-valor2 = float(input())
+valor1 = float(input("Digite o primeiro valor: "))
+valor2 = float(input("Digite o segundo valor: "))
+
 #Processamento de dados
 
 def num(valor1, valor2):
-    total = valor1*3 + valor2*2
+    total = valor1 * 3 + valor2 * 2
     return total
 
 def main():
     total = num(valor1, valor2)
-    print(f'{total:.2f}')
+    
+    #Saída de dados
+    print(f"O resultado é: {total:.2f}")
 
-#Saída de dados
 if __name__ == "__main__":
     main()
