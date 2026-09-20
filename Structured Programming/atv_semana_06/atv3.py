90bd412c75020afe929896053a3622702043f991
Clenilson Willames Alves Feitosa <willamesclenilson@gmail.com>
2026-09-19 22:23:04 -0300

    Update atv3.py

--- Structured Programming/atv_semana_06/atv3.py
+++ Structured Programming/atv_semana_06/atv3.py
@@ -1,18 +1,19 @@
 #Entrada de dados
-segundos = int(input())
+segundos = int(input("Digite a quantidade de segundos: "))
 
 #Processamento de dados
 
 def tempo(segundos):
     hora = segundos // 3600
-    min = segundos %  3600 // 60
-    seg = segundos %  3600 % 60
+    min = segundos % 3600 // 60
+    seg = segundos % 3600 % 60
     return hora, min, seg
 
 def main():
     hora, min, seg = tempo(segundos)
-    print(f'{hora}:{min}:{seg}')
+    
+    #Saída de dados
+    print(f"{hora}:{min}:{seg}")
 
-#Saída de dados
 if __name__ == "__main__":
     main()
