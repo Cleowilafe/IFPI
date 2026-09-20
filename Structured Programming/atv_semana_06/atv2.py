360ab5d1986de6a1f54e30c810362bc6c0de914e
Clenilson Willames Alves Feitosa <willamesclenilson@gmail.com>
2026-09-19 22:22:40 -0300

    Enhance input and output in atv2.py
    Added user prompt and formatted output for letter code.

--- Structured Programming/atv_semana_06/atv2.py
+++ Structured Programming/atv_semana_06/atv2.py
@@ -1,11 +1,13 @@
+#Entrada de dados
+
 def codigo(letra):
- return ord(letra)
+    return ord(letra)
 
 def main():
-    letra = input()
-    print(codigo(letra))
-
-
+    letra = input("Digite uma letra: ")
+    
+    #Saída de dados
+    print(f"O código da letra {letra} é {codigo(letra)}.")
 
 if __name__ == "__main__":
     main()
