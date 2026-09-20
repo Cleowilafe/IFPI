3831625a5b15453a70ee37dab2ab46a292b1c6fd
Clenilson Willames Alves Feitosa <willamesclenilson@gmail.com>
2026-09-19 22:23:28 -0300

    Improve input prompts and output display in atv4.py
    Updated input prompts and output formatting for clarity.

--- Structured Programming/atv_semana_06/atv4.py
+++ Structured Programming/atv_semana_06/atv4.py
@@ -1,26 +1,26 @@
-
 #Entrada de dados
-num1 = int(input())
-num2 = int(input())
-num3 = int(input())
-num4 = int(input())
-num5 = int(input())
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+num3 = int(input("Digite o terceiro número: "))
+num4 = int(input("Digite o quarto número: "))
+num5 = int(input("Digite o quinto número: "))
 
 #Processamento de dados
 
 def orgg(num1, num2, num3, num4, num5):
-    med = (num1+ num2+ num3+ num4+ num5)/5
+    med = (num1 + num2 + num3 + num4 + num5) / 5
     maxi = max(num1, num2, num3, num4, num5)
     minn = min(num1, num2, num3, num4, num5)
 
-    return med , maxi , minn
+    return med, maxi, minn
 
 def main():
     med, maxi, minn = orgg(num1, num2, num3, num4, num5)
-    print(maxi)
-    print(minn)
-    print(med)
+    
+    #Saída de dados
+    print(f"Maior: {maxi}")
+    print(f"Menor: {minn}")
+    print(f"Média: {med}")
 
-#Saída de dados
 if __name__ == "__main__":
     main()
